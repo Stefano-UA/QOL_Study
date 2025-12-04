7446e3bce8e31529cc32ecc74a7636a307556245
Carlos <110690378+csalas-alarcon@users.noreply.github.com>
2025-12-04 18:02:18 +0100

    Actualización Main Script
    Lo dicho

--- kettle/pollution/iteradorArchivosCSV.sh
+++ kettle/pollution/iteradorArchivosCSV.sh
@@ -2,6 +2,7 @@
 
 scandir="../../data/pollution"
 
+# PRIMER LOOP- FORMATEO y RATIOS
 for region in "$scandir"/*/; do
     nom_region="$(basename "$region")"
     for file in "$region"/*.csv; do
@@ -16,22 +17,43 @@ for region in "$scandir"/*/; do
             file="$cleaned"
         fi 
 
-        echo "Processing: $file"
+        echo "Procesando: $file"
 
         # Procesando con python:
-#        echo "CRIBADO INICIAL:"
-#        python3 cribadoInicial.py "$(basename "$region")" "$(realpath "$file")"
-#        echo "FORMATEO: COLUMNAS"
-#        if [ "$nom_region" == "castilla_mancha" ]; then
-#            echo "JALIBU"
-#            python3 formateadorLaMancha.py "$(realpath "$file")"
-#        else
-#            echo "NO PASA NADA"
-#            python3 formateador.py "$(basename "$region")" "$(realpath "$file")"
-#        fi
-#        echo "POST_CAMBIOS"
-#        python3 cribadoFormateado.py "$(basename "$region")" "$(realpath "$file")"
-#        echo "CALCULANDO RATIOS:"
+        echo "CRIBADO INICIAL:"
+        python3 cribadoInicial.py "$nom_region" "$(realpath "$file")"
+
+        echo "FORMATEO: COLUMNAS"
+        if [ "$nom_region" == "castilla_mancha" ]; then
+            python3 formateadorLaMancha.py "$(realpath "$file")"
+        else
+            echo "NO PASA NADA"
+            python3 formateador.py "$nom_region" "$(realpath "$file")"
+        fi
+
+        echo "POST_CAMBIOS"
+        python3 cribadoFormateado.py "$nom_region" "$(realpath "$file")"
+
+        echo "CALCULANDO RATIOS:"
         python3 ratioMaker.py "$nom_region" "$(realpath "$file")"
     done
 done
+
+# SEGUNDO LOOP - ARREGLO RATOS
+for region in "$scandir"/*/; do 
+    python3 inferenciaVecinos.py "$region"
+done 
+
+# TERCER LOOP - INFERENCIA DE DATOS
+for region in "$scandir"/*/; do 
+    nom_region="$(basename "$region")"
+    for file in "$region"/*.csv; do 
+
+        [ -e "$file" ] || continue 
+
+        echo "INFERENCIA PM25 en \n$file"
+        python3 inferencia25.py "$nom_region" "$(realpath "$file")"
+        echo "INFERENCIA CONTAMINANTES en \n$file"
+        python3 inferenciaContaminantes.py "$nom_region" "$(realpath "$file")"
+    done 
+done 
